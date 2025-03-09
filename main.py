d3867d840d78316594b3050e7f8bacc4eb750458
Mockward91 <christophermock91@gmail.com>
2025-03-08 17:32:12 -0800

    "changed main.py to run program"

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    path = "frankenstein.txt"
+    text = get_book_text(path)
+    print(text)
+
+main()
